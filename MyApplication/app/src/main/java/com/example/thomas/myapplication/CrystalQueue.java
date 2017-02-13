110b57b2b29cb16e1be01f830bc0ed98132aa9d9
Sabastian Mugazambi <mugazambi@gmail.com>
2017-02-13 16:37:49 -0600

    change the queue inputs

--- MyApplication/app/src/main/java/com/example/thomas/myapplication/CrystalQueue.java
+++ MyApplication/app/src/main/java/com/example/thomas/myapplication/CrystalQueue.java
@@ -6,20 +6,20 @@ package com.example.thomas.myapplication;
 
 public class CrystalQueue<T>  {
 
-    private T[] x_arr, y_arr;
+    private int[] x_arr, y_arr;
 
     private int total, first, next;
 
     public CrystalQueue()
     {
-        x_arr = (T[]) new Object[2];
-        y_arr = (T[]) new Object[2];
+        x_arr = new int[2];
+        y_arr = new int[2];
     }
 
     private void resize(int capacity)
     {
-        T[] x_tmp = (T[]) new Object[capacity];
-        T[] y_tmp = (T[]) new Object[capacity];
+        int[] x_tmp = new int[capacity];
+        int[] y_tmp = new int[capacity];
 
         for (int i = 0; i < total; i++) {
             x_tmp[i] = x_arr[(first + i) % x_arr.length];
@@ -33,15 +33,15 @@ public class CrystalQueue<T>  {
     }
 
     //enque a pair of x and y coodinates
-    public int enqueue(T[] elements)
+    public int enqueue(IntPair elements)
     {
         if (x_arr.length == total) {
             resize(x_arr.length * 2);
             resize(y_arr.length * 2);
 
         }
-        x_arr[next++] = elements[0];
-        x_arr[next++] = elements[1];
+        x_arr[next++] = elements.x;
+        x_arr[next++] = elements.y;
 
         if (next == x_arr.length) next = 0;
         total++;
@@ -49,15 +49,13 @@ public class CrystalQueue<T>  {
     }
 
     //returns a pair of x,y coodinates
-    public T[] dequeue()
+    public IntPair dequeue()
     {
         if (total == 0) throw new java.util.NoSuchElementException();
-        T[] pop = (T[]) new Object[2];
 
-        pop[0] = x_arr[first];
-        pop[0] = y_arr[first];
-        x_arr[first] = null;
-        y_arr[first] = null;
+        IntPair pop = new IntPair(x_arr[first],y_arr[first]);
+//        x_arr[first] = null;
+//        y_arr[first] = null;
 
         if (++first == x_arr.length) first = 0;
 
---
+++ MyApplication/app/src/main/java/com/example/thomas/myapplication/CrystalTransform.java
@@ -0,0 +1,20 @@
+package com.example.thomas.myapplication;
+
+/**
+ * Created by Sabastian on 2/12/17.
+ */
+
+public class CrystalTransform {
+
+
+    double[] getTranslationVec() {
+        double[] vec = new double[2];
+    return vec;
+    }
+
+    double[] getRotationAxis() {
+        double[] vecRot = new double[3];
+        return vecRot;
+    }
+
+}
