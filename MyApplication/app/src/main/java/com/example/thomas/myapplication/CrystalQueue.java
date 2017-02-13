8efc5d68b85164b61b969026d9278018f36cbfd3
Sabastian Mugazambi <mugazambi@gmail.com>
2017-02-13 16:52:09 -0600

    ints on enqueue

--- MyApplication/app/src/main/java/com/example/thomas/myapplication/CrystalQueue.java
+++ MyApplication/app/src/main/java/com/example/thomas/myapplication/CrystalQueue.java
@@ -13,35 +13,35 @@ public class CrystalQueue<T>  {
     public CrystalQueue()
     {
         x_arr = new int[2];
-        y_arr = new int[2];
+        //y_arr = new int[2];
     }
 
     private void resize(int capacity)
     {
         int[] x_tmp = new int[capacity];
-        int[] y_tmp = new int[capacity];
+        //int[] y_tmp = new int[capacity];
 
         for (int i = 0; i < total; i++) {
             x_tmp[i] = x_arr[(first + i) % x_arr.length];
-            y_tmp[i] = y_arr[(first + i) % x_arr.length];
+            //y_tmp[i] = y_arr[(first + i) % x_arr.length];
         }
 
         x_arr = x_tmp;
-        y_arr = y_tmp;
+        //y_arr = y_tmp;
         first = 0;
         next = total;
     }
 
     //enque a pair of x and y coodinates
-    public int enqueue(IntPair elements)
+    public int enqueue(int x, int y)
     {
         if (x_arr.length == total) {
             resize(x_arr.length * 2);
             resize(y_arr.length * 2);
 
         }
-        x_arr[next++] = elements.x;
-        x_arr[next++] = elements.y;
+        x_arr[next++] = 100000*x+y;
+        //x_arr[next++] = y;
 
         if (next == x_arr.length) next = 0;
         total++;
@@ -49,11 +49,11 @@ public class CrystalQueue<T>  {
     }
 
     //returns a pair of x,y coodinates
-    public IntPair dequeue()
+    public int dequeue()
     {
         if (total == 0) throw new java.util.NoSuchElementException();
 
-        IntPair pop = new IntPair(x_arr[first],y_arr[first]);
+        int pop = x_arr[first];
 //        x_arr[first] = null;
 //        y_arr[first] = null;
 
@@ -61,7 +61,7 @@ public class CrystalQueue<T>  {
 
         if (--total > 0 && total == x_arr.length / 4) {
             resize(x_arr.length / 2);
-            resize(y_arr.length / 2);
+            //resize(y_arr.length / 2);
         }
         return pop;
     }
